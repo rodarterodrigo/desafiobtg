6b506a6de35d5d0759ea368b4c1d5e8b570597a1
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2021-01-11 00:14:42 -0300

    ajuste

--- desafiobtg/lib/modules/convert/external/datasources/convert_currency_datasource.dart
+++ desafiobtg/lib/modules/convert/external/datasources/convert_currency_datasource.dart
@@ -13,6 +13,6 @@ class ConvertCurrencyDatasource implements IConvertCurrencyDataSource{
   @override
   Future<ConvertModel> convertCurrency(Currency from, Currency to) async{
     final response = await dio.get("${Settings.baseUrlPrefix}/live?access_key=${Settings.ApiKey}&currencies=${from.currency},${to.currency}&source=USD&format=1");
-    return response.statusCode == 200.0? ConvertModel.toConvert(response.data['quotes'] as Map): throw DataSourceError();
+    return response.statusCode == 200? ConvertModel.toConvert(response.data['quotes'] as Map): throw DataSourceError();
   }
 }
