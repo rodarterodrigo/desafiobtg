ebb8435f6025f39d44b81a0ed2dacb0bd6a8a8f5
Rodrigo Morais Rodarte <rodarte.rodrigo@hotmail.com>
2021-03-03 16:12:29 -0300

    ajuste no tamanho do nome da moeda no card

--- desafiobtg/lib/modules/convert/presenter/shared/widgets/custom_card.dart
+++ desafiobtg/lib/modules/convert/presenter/shared/widgets/custom_card.dart
@@ -19,42 +19,46 @@ class _CurrencyCardState extends State<CurrencyCard> {
       child: Card(
         elevation: 10,
         child: AnimatedContainer(
-            duration: Duration(milliseconds: 400),
-            height: 100,
-            width: MediaQuery.of(context).size.width /1.1,
-            decoration: BoxDecoration(
-              border: Border.all( color: Colors.teal, width: 2, style: BorderStyle.solid),
-              color: widget.currency.isTaped? Colors.teal: Colors.white,
-              // color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(4)),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  widget.currency.currency,
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: widget.currency.isTaped? Colors.white: Colors.teal,
-                    // color:  Colors.teal
-                  ),
+          duration: Duration(milliseconds: 400),
+          height: 100,
+          width: MediaQuery.of(context).size.width / 1.1,
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: Colors.teal, width: 2, style: BorderStyle.solid),
+            color: widget.currency.isTaped ? Colors.teal : Colors.white,
+            // color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.currency.currency,
+                style: TextStyle(
+                  fontSize: 35,
+                  color: widget.currency.isTaped ? Colors.white : Colors.teal,
+                  // color:  Colors.teal
                 ),
-                SizedBox(height: 10,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      widget.currency.name,
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: widget.currency.isTaped? Colors.white: Colors.teal,
-                          // color:  Colors.teal
-                      ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    widget.currency.name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color:
+                          widget.currency.isTaped ? Colors.white : Colors.teal,
+                      // color:  Colors.teal
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
